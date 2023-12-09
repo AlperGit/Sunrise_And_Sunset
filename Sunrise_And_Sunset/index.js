abc184f18f7b50d5e735cf0dace8ef26007b456d
Alper <47501174+AlperGit@users.noreply.github.com>
2023-12-09 22:48:20 +0100

    Added first draft for working api communication

--- Sunrise_And_Sunset/index.js
+++ Sunrise_And_Sunset/index.js
@@ -1,15 +1,59 @@
 import express from "express"
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
+
+const API_URL = "https://api.sunrise-sunset.org/json";
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(express.static("node_modules/bootstrap-icons/font"));
 
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+app.post("/", async (req, res) =>
+{
+    //Get current lat and longitude from request
+    var lat = req.body.latitude;
+    var long = req.body.longitude;
+
+    //Get current date from request
+    var day = req.body.day;
+    var month = req.body.month;
+    var year = req.body.year;
+
+    //Check if day month and year are full
+    if(day == "" || month == "" || year == "")
+    {
+        //Use axios to get the lat and long without the date
+        try {
+            const response = await axios.get(API_URL, {
+                lat : lat,
+                long : long
+            });
+
+            console.log(response.data);
+            console.log(response.data.results.sunrise);
+            res.render("index.ejs", {
+                sunrise : response.data.results.sunrise
+            });
+
+        } catch (error) {
+            console.log("error");
+            res.render("index.ejs");
+        }
+    }
+    else
+    {
+        console.log("defined");
+    }
 });
 
 app.listen(port, () => {
